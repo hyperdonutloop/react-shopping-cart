b0667a273bd7835038f4863e4536e0db7443518d
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-04 14:19:53 -0800

    completed step two and created product context

---
+++ src/Contexts/ProductContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+ export const ProductContext = createContext();
+
+ //Step 2: Created Product Context ✅
+ // Imported createContext and assigned that to ProductContext
